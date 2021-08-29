b454802e32badc1f6cf203a676388cea18bd2984
mamok19 <89607228+mamok19@users.noreply.github.com>
2021-08-29 12:01:15 +0300

    test two is finished a function that caculate quadratic equation

--- pracrice.js
+++ pracrice.js
@@ -5,6 +5,14 @@ function digitSum(num) {
         sum += num % 10
         num = num/10
     }
-    return sum
+    console.log(sum);
+  }
+
+  function quadratic_equation() {
+      let a = 1; b = -6; c = 9;
+      let solutionA = solutionB = 0
+      solutionA=(-b + Math.sqrt(b*b - 4 * a * c))/2*a
+      solutionA=(-b - Math.sqrt(b*b - 4 * a * c))/2*a
+      console.log(solutionA, solutionB)
   }
   
