bc3e75a80b6d07593324ee210a28adae829fb9f2
mamok19 <89607228+mamok19@users.noreply.github.com>
2021-08-29 12:08:05 +0300

    task 3 complete caculate absulute number

--- pracrice.js
+++ pracrice.js
@@ -8,11 +8,19 @@ function digitSum(num) {
     console.log(sum);
   }
 
-  function quadratic_equation() {
+function quadratic_equation() {
       let a = 1; b = -6; c = 9;
       let solutionA = solutionB = 0
       solutionA=(-b + Math.sqrt(b*b - 4 * a * c))/2*a
       solutionA=(-b - Math.sqrt(b*b - 4 * a * c))/2*a
       console.log(solutionA, solutionB)
-  }
+}
+
+function absuluteNumber(num) {
+    let absulute = 0;
+    absulute =num * num
+    absulute /= num
+    console.log(absulute)
+
+}
   
