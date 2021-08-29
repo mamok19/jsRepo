c7356d084f6f8410910e9663c03493afbba7df4d
mamok19 <89607228+mamok19@users.noreply.github.com>
2021-08-29 12:59:28 +0300

    fixing erors and trying diffrent numbers

--- pracrice.js
+++ pracrice.js
@@ -1,27 +1,31 @@
 function digitSum(num) {
-    num= 123
-    let sum =0
-    while (num > 1)
+    num= 123;
+    let sum =0;
+    while (num >= 1)
     {
-        sum += num % 10
-        num = num/10
+        sum += (num % 10);
+        num /= 10;
+        num = Math.floor(num);
     }
     console.log(sum);
-  }
+}
 
 function quadratic_equation() {
       let a = 1; b = -6; c = 9;
-      let solutionA = solutionB = 0
-      solutionA=(-b + Math.sqrt(b*b - 4 * a * c))/2*a
-      solutionA=(-b - Math.sqrt(b*b - 4 * a * c))/2*a
-      console.log(solutionA, solutionB)
+      let solutionA = solutionB = 0;
+      solutionA=(-b + Math.sqrt(b*b - 4 * a * c))/2*a;
+      solutionA=(-b - Math.sqrt(b*b - 4 * a * c))/2*a;
+      console.log(solutionA,  solutionB);
 }
 
 function absuluteNumber(num) {
+    num = -8;
     let absulute = 0;
-    absulute =num * num
-    absulute /= num
-    console.log(absulute)
+    absulute = num * num;
+    absulute = Math.sqrt(absulute);
+    console.log(absulute);
 
 }
-  
+  digitSum();
+  quadratic_equation();
+  absuluteNumber();
