c8ac648cb7e428d42f0a13f52a8d0a8c169f409f
mamok19 <89607228+mamok19@users.noreply.github.com>
2021-08-30 18:48:39 +0300

    creating a new page and declaring varibels

---
+++ school.js
@@ -0,0 +1,17 @@
+function studets_grades()
+{
+    const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92]
+    let half_range;
+    let median;
+    let organized_Grades = grades.sort();
+//     if (grades.length()%2 === 1){
+//         median = organized_Grades[((organized_Grades.length())/2)+0.5];
+//         half_range = organized_Grades[((organized_Grades.length())/2)+0.5] - organized_Grades[organized_Grades.length()-1];
+//     }
+//     else {
+//         median = organized_Grades[(grades.length())/2];
+//         half_range = organized_Grades[((organized_Grades.length())/2)] - organized_Grades[organized_Grades.length()-1];
+//     }
+//     console.log("stats: \n","    Range: ", (organized_Grades[0]- organized_Grades[organized_Grades.length()-1]),"\n","    Median: ",median, "\n", "    Half Range: ", half_range);
+// }
+studets_grades();
